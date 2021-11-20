e259100e7bac717ea430289eb7733c6566ae7b67
LDC.ICE <ikool009@hotmail.com>
2021-11-20 17:10:10 +0700

    import page LoginPage Done

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <!-- here  -->
-    <div>
-      <b-button>Button</b-button>
-      <b-button variant="danger">Button</b-button>
-      <b-button variant="success">Button</b-button>
-      <b-button variant="outline-primary">Button</b-button>
-    </div>
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+      <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    
   },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -3,12 +3,24 @@ import App from './App.vue'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import VueRouter from 'vue-router'
+import LoginPage from "@/components/LoginPage/index.vue"
 
 Vue.config.productionTip = false
-
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
+Vue.use(VueRouter)
+const routes = [
+  {
+    path: "/",
+    component: LoginPage
+  }
+]
+const router  = new VueRouter({
+  routes
+})
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
