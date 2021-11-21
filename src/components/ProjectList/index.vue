c0750b9e52968da1f70249e0261706b1ed44a44f
LDC.ICE <ikool009@hotmail.com>
2021-11-21 11:21:42 +0700

    Create Layout By flex

--- src/components/ProjectList/index.vue
+++ src/components/ProjectList/index.vue
@@ -1,6 +1,9 @@
 <template>
-  <div>
-    <h1>Project List</h1>
+  <div class="content-page">
+    <div class="container-box">
+      <div class="content-left"></div>
+      <div class="content-right"></div>
+    </div>
   </div>
 </template>
 
@@ -9,4 +12,29 @@ export default {};
 </script>
 
 <style>
+.content-page {
+  height: 100%;
+  overflow-y: auto;
+  background-color: #e7e7e7;
+  padding-left: 20px;
+  padding-right: 20px;
+}
+
+.container-box {
+  display: flex;
+  height: inherit;
+}
+
+.content-left {
+  padding: 10px;
+  flex: 3;
+  height: inherit;
+}
+
+.content-right {
+  flex: 1;
+  padding: 10px;
+  height: inherit;
+}
+
 </style>
