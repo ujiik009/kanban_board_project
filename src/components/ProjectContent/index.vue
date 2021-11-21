89f06015b92cc39fe657bdd80d19f3493a50f03b
LDC.ICE <ikool009@hotmail.com>
2021-11-21 15:46:00 +0700

    Create tabs in ProjectContent

--- src/components/ProjectContent/index.vue
+++ src/components/ProjectContent/index.vue
@@ -1,17 +1,46 @@
 <template>
-    <div>
-        <h1>
-            Project Content
-        </h1>
+  <div class="content-page">
+    <div class="content-body">
+      <b-tabs content-class="mt-3">
+        <b-tab title="Kanban Board" active>
+          <div>Kanban Board</div>
+        </b-tab>
+        <b-tab title="Member">
+          <div>Member</div>
+        </b-tab>
+      </b-tabs>
     </div>
+  </div>
 </template>
+
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      project_id: this.$route.params.project_id,
+    };
+  },
+};
 </script>
 
 <style>
+.content-page {
+  height: 100%;
+  overflow-y: auto;
+  background-color: #e7e7e7;
+  padding-left: 20px;
+  padding-right: 20px;
+}
 
+.content-body {
+  margin: 20px;
+  background-color: white;
+  height: inherit;
+  border-radius: 5px;
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.2);
+  -webkit-box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.2);
+  -moz-box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.2);
+  overflow-y: auto;
+}
 </style>
